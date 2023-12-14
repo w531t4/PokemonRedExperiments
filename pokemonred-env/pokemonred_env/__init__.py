d3e65e1052a0e41261df2bdc3d82a505eda74f0e
Aaron White <w531t4@gmail.com>
2023-12-14 23:40:39 +0000

    update pokeredenv

--- pokemonred-env/pokemonred_env/__init__.py
+++ pokemonred-env/pokemonred_env/__init__.py
@@ -1,9 +1,9 @@
 from gymnasium.envs.registration import register
-from gymnasium.wrappers import AutoResetWrapper
+from gymnasium.wrappers.autoreset import AutoResetWrapper
 
 register(
      id="pokemonred_env/PokeRed-v0",
      entry_point="pokemonred_env.envs:PokeRedEnv",
      max_episode_steps=20480,
-     additional_wrappers=(AutoResetWrapper),
+     additional_wrappers=[AutoResetWrapper.wrapper_spec()],
 )
