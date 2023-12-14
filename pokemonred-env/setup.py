adf0be63e6b713a71ebf4d4fc4ca47dd9f614603
Aaron White <w531t4@gmail.com>
2023-12-14 22:15:48 +0000

    initial package build

---
+++ pokemonred-env/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="pokemonred_env",
+    version="0.0.1",
+    install_requires=[
+                      "matplotlib==3.7.1",
+                      "hnswlib==0.7.0",
+                      "numpy==1.25.0",
+                      "einops==0.6.1",
+                      "matplotlib==3.7.1",
+                      "scikit-image==0.21.0",
+                      "pyboy==1.6.9",
+                      "mediapy @ git+https://github.com/PWhiddy/mediapy.git@45101800d4f6adeffe814cad93de1db67c1bd614",
+                      "pandas==2.0.2",
+                      "gymnasium>=0.26.0",
+                      ],
+)
