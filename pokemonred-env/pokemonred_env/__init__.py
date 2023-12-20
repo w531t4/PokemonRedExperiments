86cb698c8e250c0dba0d9227158de6c0fcbf1a9b
Aaron White <w531t4@gmail.com>
2023-12-20 20:03:44 +0000

    add hyperparams

--- pokemonred-env/pokemonred_env/__init__.py
+++ pokemonred-env/pokemonred_env/__init__.py
@@ -6,15 +6,16 @@ register(
      max_episode_steps=20480,
 )
 
-hyperparams = dict(frame_stack=3,
-                   policy="CnnPolicy",
-                   n_envs=4,
-                   n_steps=20480,
-                   n_epochs=4,
-                   batch_size=128,
-                   n_timesteps=0.0000001,
-                   learning_rate=0.0003,
-                   clip_range=0.2,
-                   vf_coef=0.5,
-                   ent_coef=0.0,
-                   )
+hyperparams = {"PokeRed-v0": dict(frame_stack=3,
+                                  policy="CnnPolicy",
+                                  n_envs=4,
+                                  n_steps=20480,
+                                  n_epochs=4,
+                                  batch_size=128,
+                                  n_timesteps=0.0000001,
+                                  learning_rate=0.0003,
+                                  clip_range=0.2,
+                                  vf_coef=0.5,
+                                  ent_coef=0.0,
+                                  )
+               }
